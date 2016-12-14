dc6f068ea59c018390a83dc4f9ebc14ad9313677
jwalle <jwalle@student.42.fr>
2016-12-14 17:09:55 +0100

    javascript canvas draw

--- content.php
+++ content.php
@@ -27,12 +27,21 @@
 
     <script>
         var video = document.getElementById('video');
+        var canvas = document.getElementById('canvas');
+        var photo = document.getElementById('photo');
+        var context = canvas.getContext('2d');
+
 
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
         {
             navigator.mediaDevices.getUserMedia({video : true}).then(function(stream) {
                 video.src = window.URL.createObjectURL(stream);
                 video.play();
+            });
+            document.getElementById("snap").addEventListener("click", function () {
+                context.drawImage(video, 0, 0, 640, 480);
+                var data = canvas.toDataURL('image/png');
+                photo.setAttribute('src', data);
             });
         }
     </script>
